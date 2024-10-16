61087a25f81ca297845e5db45532b88f45ddf8c7
Shakeel <shakeeluetp1041@gmail.com>
2024-10-16 15:21:52 +0200

    sffjfjhjfh

--- test.py
+++ test.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 initiated on local feature branch
 modified on remote feature branch
 data sent from local remote_featute to remote master
-
-=======
-print("start again")
-print("to chack for the lagging status on local")
->>>>>>> origin/master
